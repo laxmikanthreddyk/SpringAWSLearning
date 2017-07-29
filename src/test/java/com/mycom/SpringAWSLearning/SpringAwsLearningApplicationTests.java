86a0cb5eeb168513d2c7948c50d8660b8097ec5e
laxmikanthreddyk <laxmikanthreddyk@gmail.com>
2017-07-29 01:40:21 -0400

    Added Logging component

--- src/test/java/com/mycom/SpringAWSLearning/SpringAwsLearningApplicationTests.java
+++ src/test/java/com/mycom/SpringAWSLearning/SpringAwsLearningApplicationTests.java
@@ -2,15 +2,32 @@ package com.mycom.SpringAWSLearning;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.mycom.SpringAWSLearning.web.i18n.I18NService;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest
-public class SpringAwsLearningApplicationTests {
+public class SpringAwsLearningApplicationTests 
+{
+	
+	@Autowired
+	private I18NService i18NService;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void testMessageByLocaleService() throws Exception
+    {
+    	 String expectedMessage = "Bootstrap starter template";
+    	 String messageId = "index.main.callout";
+    	 String actual = i18NService.getMessage(messageId);
+    	 Assert.assertEquals("The actual and Expected Message Don't Match", expectedMessage, actual);
+    }
 
 }
